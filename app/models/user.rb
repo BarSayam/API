cb7166981e2ed811c18dbec91d0ab961cc1a5ede
SayamBar <sayam.bar@bitcanny.com>
2023-01-05 17:21:24 +0530

    API & pagination without gem

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  validates :email, presence: true
+  validates :password, presence: true
+
+  validates :email, format: URI::MailTo::EMAIL_REGEXP
+  
+  # the authenticate method from devise documentation
+  def self.authenticate(email, password)
+    user = User.find_for_authentication(email: email)
+    user&.valid_password?(password) ? user : nil
+  end
+end
